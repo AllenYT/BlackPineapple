bfd26e5d37d954828c122849d6e8f7b0651fc75a
AllenYT <1837266088@qq.com>
2020-07-20 11:46:09 +0800

    新功能

--- BlackPineapple/pages/index/index.js
+++ BlackPineapple/pages/index/index.js
@@ -1,38 +1,17 @@
 //Page Object
 Page({
   data: {
-    
+    swiperList:[]
   },
   //options(Object)
   onLoad: function(options){
-    
-  },
-  onReady: function(){
-    
-  },
-  onShow: function(){
-    
-  },
-  onHide: function(){
-
-  },
-  onUnload: function(){
-
-  },
-  onPullDownRefresh: function(){
-
-  },
-  onReachBottom: function(){
-
-  },
-  onShareAppMessage: function(){
-
-  },
-  onPageScroll: function(){
-
-  },
-  //item(index,pagePath,text)
-  onTabItemTap:function(item){
-
+    wx.request({
+      url: 'https://api-hmugo-web.itheima.net/api/public/v1/home/swiperdata',
+      success: (result) => {
+        this.setData({
+          swiperList:result.data.message
+        })
+      }
+    })
   }
 });
