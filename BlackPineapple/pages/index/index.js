96dadda6a2192630b0735a812664ae54b726c343
AllenYT <1837266088@qq.com>
2020-07-22 14:14:26 +0800

    新增分类界面

--- BlackPineapple/pages/index/index.js
+++ BlackPineapple/pages/index/index.js
@@ -1,17 +1,47 @@
-//Page Object
+import { request } from "../../request/index.js";
 Page({
   data: {
-    swiperList:[]
+    swiperList: [],
+    catesList: [],
+    floorList: []
   },
   //options(Object)
-  onLoad: function(options){
-    wx.request({
-      url: 'https://api-hmugo-web.itheima.net/api/public/v1/home/swiperdata',
-      success: (result) => {
+  onLoad: function (options) {
+    // wx.request({
+    //   url: 'https://api-hmugo-web.itheima.net/api/public/v1/home/swiperdata',
+    //   success: (result) => {
+    //     this.setData({
+    //       swiperList:result.data.message
+    //     })
+    //   }
+    // })
+    this.getSwiperList();
+    this.getCateList();
+    this.getFloorList();
+  },
+  getSwiperList() {
+    request({ url: "https://api-hmugo-web.itheima.net/api/public/v1/home/swiperdata" })
+      .then(result => {
         this.setData({
-          swiperList:result.data.message
+          swiperList: result.data.message
         })
-      }
-    })
+      })
+  },
+  getCateList() {
+    request({ url: "https://api-hmugo-web.itheima.net/api/public/v1/home/catitems" })
+      .then(result => {
+        this.setData({
+          catesList: result.data.message
+        })
+      })
+  },
+  getFloorList() {
+    request({ url: "https://api-hmugo-web.itheima.net/api/public/v1/home/floordata" })
+      .then(result => {
+        this.setData({
+          floorList: result.data.message
+        })
+      })
   }
 });
+wx
